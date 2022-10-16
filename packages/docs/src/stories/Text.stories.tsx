e8ca577e0fa63535a194abf78cae781d972cad34
gabriel-github <gabrielferrsantos201@gmail.com>
2022-10-16 01:38:37 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Text, TextProps } from "@ignite-ui/react";
+
+export default {
+  title: "Typography/Text",
+  component: Text,
+  args: {
+    children:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa iure magnam tempora? Voluptatibus corporis accusamus architecto recusandae maxime tempora laudantium sed repudiandae culpa. Consectetur hic itaque temporibus corrupti cupiditate quibusdam.",
+    size: "md",
+  },
+  argTypes: {
+    size: {
+      options: [
+        "xxs",
+        "xs",
+        "sm",
+        "md",
+        "lg",
+        "xl",
+        "2xl",
+        "4xl",
+        "5xl",
+        "6xl",
+        "7xl",
+        "8xl",
+        "9xl",
+      ],
+      control: {
+        type: "inline-radio",
+      },
+    },
+  },
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: "Strong text",
+    as: "strong",
+  },
+};
